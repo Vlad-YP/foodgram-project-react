1154573a67d236c8bccb0baec89cb81f97889dc3
Владислав Максимов <vlsdmaximov@yandex.com>
2022-08-26 22:15:22 +0600

    first rew. fix settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,11 +5,11 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '7f-f%@ep&kln=6p2l@)%i#%ecv)-al@dlbp!s3#i(bb^(f+3(8'
+SECRET_KEY = os.getenv('SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*')
 
 
 INSTALLED_APPS = [
